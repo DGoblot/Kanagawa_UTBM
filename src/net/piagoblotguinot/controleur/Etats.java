4c6747bc73304489cf6f74feee9abed5d14c57c0
UgoPIA <90686290+UgoPIA@users.noreply.github.com>
2022-01-08 01:47:34 +0100

    Update Etats.java

--- src/net/piagoblotguinot/controleur/Etats.java
+++ src/net/piagoblotguinot/controleur/Etats.java
@@ -1,5 +1,7 @@
 package net.piagoblotguinot.controleur;
-
+/*
+        Enumération des états possibles du jeu
+*/
 public enum Etats
 {
     MENU, HISTORIQUE, NOUVELLE_PARTIE, PARTIE_EN_COURS, FIN_JEU
